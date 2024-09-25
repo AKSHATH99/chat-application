6c685ae8fa6b09406ff8dbd3413680d8e8e51948
Akshath P <akshathpkk@gmail.com>
2024-09-25 23:56:17 +0530

    Added online users

--- frontend/src/components/MessageInterface.js
+++ frontend/src/components/MessageInterface.js
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 import debounce from "lodash.debounce";
 
 const socket = io("http://localhost:3000");
-const username = nanoid(4);
+const NickName = nanoid(4);
 
 function ChatApp() {
   const [msg, setMsg] = useState('');
@@ -12,6 +12,8 @@ function ChatApp() {
   const [connectionMessage, setConnectionMessage] = useState('');
   const [isTyping, setIsTyping] = useState(false); // To show "user is typing" notification.
   const [typingUser, setTypingUser] = useState(''); // User who is typing.
+  // const [onlineUsers , setOnlineUsers] = useState([]);
+  const [newUser ,setNewUser ] = useState("")
 
   //For nicknames
   const [username , setUsername] = useState("")
@@ -46,6 +48,8 @@ function ChatApp() {
     socket.on("connectionMessage", ( message) => {
       console.log(message);
       setConnectionMessage(message);
+      socket.emit("OnlineNotification", { NickName });
+      setNewUser(NickName)
     });
 
     socket.on("typingNotification", (data) => {
@@ -57,6 +61,11 @@ function ChatApp() {
         setIsTyping(false);
       }, 3000);
     });
+
+    socket.on("newUserOnline",(data)=>{
+      console.log("new user just connected bruhh",data);
+      
+    })
 
     // Clean up listeners on unmount
     return () => {
@@ -77,15 +86,17 @@ function ChatApp() {
         <h1 className="text-4xl font-bold text-center mb-6">CHATTER 💬🗨️ </h1>
 
         <span className=" flex m-10 ">
-          <p className="font-bold">USERNAME : </p>{" "}
+          <p className="font-bold mt-1">USERNAME : </p>{" "}
           <input
-            onChange={(e)=>setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             value={username}
             placeholder="Enter your username"
-            className="rounded-md ml-5 p-2 text-black"
+            className="rounded-l-md ml-5 p-2 text-black"
             type="text"
           ></input>
-          <button className="bg-green-500 text-white p-2 rounded-lg ml-11 w-28" >Submit</button>
+          <button className="bg-green-500 text-white p-2 rounded-r-md  w-28">
+            Submit
+          </button>
         </span>
         {/* Connection Message */}
         {connectionMessage ? (
@@ -99,43 +110,60 @@ function ChatApp() {
           ""
         )}
 
-         {/* Typing Notification */}
-         {isTyping && (
-          <div className="text-yellow-300 mb-2">
-            {typingUser} is typing...
-          </div>
+        {/* Typing Notification */}
+        {isTyping && (
+          <div className="text-yellow-300 mb-2">{typingUser} is typing...</div>
         )}
 
-        {/* Chat Messages */}
-        <div className="bg-gray-700 p-4 rounded-lg mb-4 h-64 overflow-y-auto">
-          {chat.map((payload, index) => (
-            <p key={index} className="mb-2">
-              <span className="font-semibold text-indigo-400">
-                {payload.username}
-              </span>
-              : {payload.msg}
-            </p>
-          ))}
-        </div>
 
-        {/* Chat Input */}
-        <form onSubmit={sendChat} className="flex">
-          <input
-            type="text"
-            name="chat"
-            placeholder="
-        Your message here"
-            value={msg}
-            onChange={HandleInputChage}
-            className="w-full p-2 text-gray-900 rounded-l-lg focus:outline-none"
-          />
-          <button
-            type="submit"
-            className="bg-indigo-500 hover:bg-indigo-600 text-white p-2 rounded-r-lg transition-colors"
-          >
-            SEND
-          </button>
-        </form>
+        <div className="flex">
+
+          <div className="p-3">
+            <h1 className="text-green-400 font-bold text-xl">Online Users</h1>
+
+            <ul className="mt-4 flex-col flex-wrap">
+              
+              { NickName?<li className="mt-5 flex hover:bg-gray-600 w-full  hover:cursor-pointer p-1"><img className="w-5 h-5 mr-2   " src="/images/tick.png"/>{NickName}</li>:""}
+              <li className="mt-5 flex hover:bg-gray-600 w-full  hover:cursor-pointer p-1"><img className="w-5 h-5 mr-2   " src="/images/tick.png"/>Arjun</li>
+              <li className="mt-5 flex hover:bg-gray-600 w-full  hover:cursor-pointer p-1"><img className="w-5 h-5 mr-2   " src="/images/tick.png"/>Adwaith</li> 
+              <li className="mt-5 flex hover:bg-gray-600 w-full  hover:cursor-pointer p-1"><img className="w-5 h-5 mr-2   " src="/images/tick.png"/>Amith</li>
+              <li className="mt-5 flex hover:bg-gray-600 w-full  hover:cursor-pointer p-1"><img className="w-5 h-5 mr-2   " src="/images/tick.png"/>Tony Stark</li>
+              
+            </ul>
+          </div>
+          <div className="ml-10">
+            {/* Chat Messages */}
+            <div className="bg-gray-700 p-4 rounded-lg mb-4 h-64 overflow-y-auto w-[500px]">
+              {chat.map((payload, index) => (
+                <p key={index} className="mb-2">
+                  <span className="font-semibold text-indigo-400">
+                    {payload.username}
+                  </span>
+                  : {payload.msg}
+                </p>
+              ))}
+            </div>
+
+            {/* Chat Input */}
+
+            <form onSubmit={sendChat} className="flex">
+              <input
+                type="text"
+                name="chat"
+                placeholder="Your message here"
+                value={msg}
+                onChange={HandleInputChage}
+                className="w-full p-2 text-gray-900 rounded-l-lg focus:outline-none"
+              />
+              <button
+                type="submit"
+                className="bg-indigo-500 hover:bg-indigo-600 text-white p-2 rounded-r-lg transition-colors"
+              >
+                SEND
+              </button>
+            </form>
+          </div>
+        </div>
       </header>
     </div>
   );
